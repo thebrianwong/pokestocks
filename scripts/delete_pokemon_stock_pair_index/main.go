bd9daacccee5baeeb17b7ecb71943f0c5281066b
Brian Wong <brianw7218@gmail.com>
2024-09-04 13:49:26 -0700

    Add script to delete PSP index

---
+++ scripts/delete_pokemon_stock_pair_index/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	"log"
+	"pokestocks/utils"
+)
+
+func main() {
+	utils.LoadEnvVars("../../.env")
+	elasticClient := utils.ConnectToElastic()
+
+	_, err := elasticClient.Indices.Delete("pokemon_stock_pairs_index").Do(context.TODO())
+
+	if err != nil {
+		log.Fatalf("Error deleting pokemon_stock_pairs_index: %v", err)
+	}
+	log.Println("Successfully deleted pokemon_stock_pairs_index")
+}
