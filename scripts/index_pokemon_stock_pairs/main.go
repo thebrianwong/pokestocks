7874fbb0ecc5928904ae0c5812e43cde1553c636
Brian Wong <brianw7218@gmail.com>
2024-09-05 12:20:55 -0700

    Add script to index PSPs into Elasticsearch

---
+++ scripts/index_pokemon_stock_pairs/main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"pokestocks/utils"
+	"sync"
+
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/typedapi/core/count"
+	"github.com/jackc/pgx/v5"
+)
+
+type pokemonPayload struct {
+	Id            int64  `json:"id"`
+	Name          string `json:"name"`
+	PokedexNumber int32  `json:"pokedex_number"`
+	Type1         string `json:"type_1"`
+	Type2         string `json:"type_2"`
+}
+
+type stockPayload struct {
+	Id     int64  `json:"id"`
+	Symbol string `json:"symbol"`
+	Name   string `json:"name"`
+	Active bool   `json:"active"`
+}
+
+type pspIndexPayload struct {
+	Id           int64          `json:"id"`
+	Pokemon      pokemonPayload `json:"pokemon"`
+	Stock        stockPayload   `json:"stock"`
+	ActiveSeason bool           `json:"active_season"`
+}
+
+func getPspIndexDocCount(elasticClient *elasticsearch.TypedClient) (*(count.Response), error) {
+	count, err := elasticClient.Count().Index("pokemon_stock_pairs_index").Do(context.Background())
+
+	if err != nil {
+		return nil, err
+	}
+
+	return count, nil
+}
+
+func convertDbRowToIndexPayload(rowDataMap map[string]any) pspIndexPayload {
+	payload := pspIndexPayload{
+		Id: rowDataMap["pspId"].(int64),
+		Pokemon: pokemonPayload{
+			Id:            rowDataMap["pokemonId"].(int64),
+			Name:          rowDataMap["pokemonName"].(string),
+			PokedexNumber: rowDataMap["pokedexNumber"].(int32),
+			Type1:         rowDataMap["type1Name"].(string),
+		},
+		Stock: stockPayload{
+			Id:     rowDataMap["stockId"].(int64),
+			Symbol: rowDataMap["stockSymbol"].(string),
+			Name:   rowDataMap["stockName"].(string),
+			Active: rowDataMap["stockActive"].(bool),
+		},
+		ActiveSeason: rowDataMap["seasonActive"].(bool),
+	}
+
+	hasSecondType := rowDataMap["type2Name"]
+	if hasSecondType != nil {
+		payload.Pokemon.Type2 = rowDataMap["type2Name"].(string)
+	}
+
+	return payload
+}
+
+func indexPspPayload(elasticClient *elasticsearch.TypedClient, payload pspIndexPayload) error {
+	_, err := elasticClient.Index("pokemon_stock_pairs_index").Request(payload).Do(context.Background())
+
+	return err
+}
+
+func main() {
+	utils.LoadEnvVars("../../.env")
+	conn := utils.ConnectToDb()
+	elasticClient := utils.ConnectToElastic("../../http_ca.crt")
+
+	count, err := getPspIndexDocCount(elasticClient)
+	if err != nil {
+		log.Fatalf("Error starting PSP indexing: %v", err)
+	}
+	if count.Count > 0 {
+		log.Fatal(" Error starting PSP indexing: the index already contains PSPs")
+	}
+
+	query := `
+		SELECT 
+			psp.id AS "pspId",
+			pokemon.id AS "pokemonId",
+			pokemon."name" AS "pokemonName",
+			pokemon.pokedex_number AS "pokedexNumber",
+			pokemon_types1."type" AS "type1Name",
+			pokemon_types2."type" AS "type2Name",
+			stocks.id AS "stockId",
+			stocks.symbol AS "stockSymbol", 
+			stocks."name" AS "stockName",
+			stocks.active AS "stockActive",
+			seasons.active AS "seasonActive"
+		FROM pokemon_stock_pairs AS psp
+		INNER JOIN pokemon ON pokemon.id = psp.pokemon_id
+		INNER JOIN pokemon_types AS pokemon_types1 ON pokemon_types1.id = pokemon.type_1_id
+		LEFT JOIN pokemon_types AS pokemon_types2 ON pokemon_types2.id = pokemon.type_2_id
+		INNER JOIN stocks ON stocks.id = psp.stock_id
+		INNER JOIN seasons ON seasons.id = psp.season_id
+		ORDER BY pokemon.id
+	`
+	rows, err := conn.Query(context.Background(), query)
+	if err != nil {
+		log.Fatalf("Error querying PSPs: %v", err)
+	}
+	defer rows.Close()
+
+	var payloads []pspIndexPayload
+
+	for rows.Next() {
+		queriedData, err := pgx.RowToMap(rows)
+		if err != nil {
+			log.Fatalf("Error reading individual row: %v", err)
+		}
+
+		payload := convertDbRowToIndexPayload(queriedData)
+		payloads = append(payloads, payload)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Fatalf("Error reading queried rows: %v", err)
+	}
+
+	var wg sync.WaitGroup
+	errChan := make(chan error, len(payloads))
+	defer close(errChan)
+
+	for _, payload := range payloads {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			err := indexPspPayload(elasticClient, payload)
+			if err != nil {
+				errChan <- err
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	select {
+	case err := <-errChan:
+		log.Fatalf("Error indexing PSPs into Elasticsearch: %v", err)
+	default:
+		fmt.Println("Successfully indexed PSPs into Elasticsearch")
+	}
+}
