c5dd703d84fb0b8567afa7d656bca21d26ccc47b
Brian Wong <brianw7218@gmail.com>
2024-09-16 16:18:46 -0700

    Add Redis key functions

--- redis/keys.go
+++ redis/keys.go
@@ -1,9 +1,19 @@
 package redis
 
+import "strings"
+
 const (
 	keyPrefix = "POKESTOCKS"
 )
 
 func ElasticCacheKey(searchValue string) string {
 	return keyPrefix + ":elastic#" + searchValue
+}
+
+func DbCacheKey(id string) string {
+	return keyPrefix + ":db#" + id
+}
+
+func GetIdFromDbCacheKey(key string) string {
+	return strings.Split(key, "#")[1]
 }
