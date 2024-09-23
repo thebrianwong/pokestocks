d8079285caa1ce15c115be1db70938818d682e7c
Brian Wong <brianw7218@gmail.com>
2024-09-22 21:50:39 -0700

    Implement random PSP function

---
+++ services/pokemon_stock_pair/get_random_pokemon_stock_pairs.go
@@ -0,0 +1,75 @@
+package pokemon_stock_pair
+
+import (
+	"context"
+	common_pb "pokestocks/proto/common"
+	psp_pb "pokestocks/proto/pokemon_stock_pair"
+
+	"github.com/jackc/pgx/v5"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) GetRandomPokemonStockPairs(ctx context.Context, in *psp_pb.GetRandomPokemonStockPairsRequest) (*psp_pb.GetRandomPokemonStockPairsResponse, error) {
+	db := s.DB
+
+	idQuery := `
+		SELECT id
+		FROM pokemon_stock_pairs
+	`
+
+	idsRows, err := db.Query(context.Background(), idQuery)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "error while querying PSP ids: %v", err)
+	}
+	defer idsRows.Close()
+
+	var ids []any
+
+	for idsRows.Next() {
+		var id int
+		err = idsRows.Scan(&id)
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "error converting queried PSP ids: %v", err)
+		}
+
+		ids = append(ids, id)
+	}
+
+	if err = idsRows.Err(); err != nil {
+		return nil, status.Errorf(codes.Internal, "error reading queried PSP ids: %v", err)
+	}
+
+	randomIndices := generateRandomIndices(5, len(ids))
+
+	var pspIds []any
+
+	for _, randomIndex := range randomIndices {
+		pspIds = append(pspIds, randomIndex)
+	}
+
+	query := pspQueryString() + "WHERE psp.id IN ($1, $2, $3, $4, $5)"
+	rows, err := db.Query(context.Background(), query, pspIds...)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "error while querying PSPs: %v", err)
+	}
+	defer rows.Close()
+
+	var psps []*common_pb.PokemonStockPair
+
+	for rows.Next() {
+		queriedData, err := pgx.RowToMap(rows)
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "error converting queried PSPs to maps: %v", err)
+		}
+
+		psp := convertDbRowToPokemonStockPair(queriedData)
+		psps = append(psps, psp)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, status.Errorf(codes.Internal, "error reading queried PSPs: %v", err)
+	}
+
+	return &psp_pb.GetRandomPokemonStockPairsResponse{Data: psps}, nil
+}
