e68bbdbd9eda04e3459a76bdee1911fc13188cbd
Brian Wong <brianw7218@gmail.com>
2024-09-23 10:39:02 -0700

    Refactor PSP query logic into its own function

--- services/pokemon_stock_pair/helpers.go
+++ services/pokemon_stock_pair/helpers.go
@@ -301,3 +301,72 @@ func (s *Server) searchPokemonStockPairIds(ctx context.Context, searchValue stri
 
 	return pspIds, nil
 }
+
+func (s *Server) queryPokemonStockPairs(ctx context.Context, pspIds []string) ([]*common_pb.PokemonStockPair, error) {
+	redisClient := s.RedisClient
+	redisPipeline := redisClient.Pipeline()
+
+	var psps []*common_pb.PokemonStockPair
+
+	cachedIds := []string{}
+	cachedPspsMap := map[string]*common_pb.PokemonStockPair{}
+	nonCachedIds := []string{}
+
+	for _, id := range pspIds {
+		redisPipeline.JSONGet(ctx, redis_keys.DbCacheKey(id)).Result()
+	}
+
+	results, err := redisPipeline.Exec(ctx)
+	if err == nil {
+		for _, result := range results {
+			jsonString := result.(*redis.JSONCmd).Val()
+			key := result.(*redis.JSONCmd).Args()[1]
+			id := redis_keys.GetIdFromDbCacheKey(key.(string))
+			if jsonString == "" {
+				nonCachedIds = append(nonCachedIds, id)
+			} else {
+				cachedIds = append(cachedIds, id)
+
+				var psp common_pb.PokemonStockPair
+				json.Unmarshal([]byte(jsonString), &psp)
+				cachedPspsMap[id] = &psp
+			}
+		}
+	}
+
+	if len(cachedIds) == 0 && len(nonCachedIds) == 0 {
+		// the above loop on results never occurred so the id arrays are both empty
+		utils.LogWarningError("Error querying Redis key for cached PSPs. Falling back to db", err)
+		psps, err = s.queryDbForPokemonStockPairs(ctx, pspIds)
+		if err != nil {
+			return nil, err
+		}
+	} else if len(nonCachedIds) > 0 {
+		// 1 or more cache misses require querying db
+		nonCachedPspsArr, err := s.queryDbForPokemonStockPairs(ctx, nonCachedIds)
+		if err != nil {
+			return nil, err
+		}
+
+		nonCachedPspsMap := map[string]*common_pb.PokemonStockPair{}
+		for _, nonCachedPsp := range nonCachedPspsArr {
+			nonCachedPspsMap[fmt.Sprint(nonCachedPsp.Id)] = nonCachedPsp
+		}
+
+		for _, id := range pspIds {
+			val, ok := cachedPspsMap[id]
+			if ok {
+				psps = append(psps, val)
+			} else {
+				psps = append(psps, nonCachedPspsMap[id])
+			}
+		}
+	} else {
+		// 0 cache misses
+		for _, id := range cachedIds {
+			psps = append(psps, cachedPspsMap[id])
+		}
+	}
+
+	return psps, nil
+}
--- services/pokemon_stock_pair/search_pokemon_stock_pairs.go
+++ services/pokemon_stock_pair/search_pokemon_stock_pairs.go
@@ -2,14 +2,8 @@ package pokemon_stock_pair
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	common_pb "pokestocks/proto/common"
 	psp_pb "pokestocks/proto/pokemon_stock_pair"
-	redis_keys "pokestocks/redis"
-	"pokestocks/utils"
 
-	"github.com/redis/go-redis/v9"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -19,9 +13,6 @@ func (s *Server) SearchPokemonStockPairs(ctx context.Context, in *psp_pb.SearchP
 
 	searchValue := in.SearchValue
 
-	redisClient := s.RedisClient
-	redisPipeline := redisClient.Pipeline()
-
 	pspIds, err := s.searchPokemonStockPairIds(ctx, searchValue)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "error searching for PSP ids: %v", err)
@@ -30,66 +21,9 @@ func (s *Server) SearchPokemonStockPairs(ctx context.Context, in *psp_pb.SearchP
 		return &psp_pb.SearchPokemonStockPairsResponse{Data: nil}, nil
 	}
 
-	cachedIds := []string{}
-	cachedPspsMap := map[string]*common_pb.PokemonStockPair{}
-	nonCachedIds := []string{}
-
-	for _, id := range pspIds {
-		redisPipeline.JSONGet(ctx, redis_keys.DbCacheKey(id)).Result()
-	}
-
-	results, err := redisPipeline.Exec(ctx)
-	if err == nil {
-		for _, result := range results {
-			jsonString := result.(*redis.JSONCmd).Val()
-			key := result.(*redis.JSONCmd).Args()[1]
-			id := redis_keys.GetIdFromDbCacheKey(key.(string))
-			if jsonString == "" {
-				nonCachedIds = append(nonCachedIds, id)
-			} else {
-				cachedIds = append(cachedIds, id)
-
-				var psp common_pb.PokemonStockPair
-				json.Unmarshal([]byte(jsonString), &psp)
-				cachedPspsMap[id] = &psp
-			}
-		}
-	}
-
-	var psps []*common_pb.PokemonStockPair
-
-	if len(cachedIds) == 0 && len(nonCachedIds) == 0 {
-		// the above loop on results never occurred so the id arrays are both empty
-		utils.LogWarningError("Error querying Redis key "+redis_keys.ElasticCacheKey(searchValue)+" for cached JSON. Falling back to db", err)
-		psps, err = s.queryDbForPokemonStockPairs(ctx, pspIds)
-		if err != nil {
-			return nil, status.Errorf(codes.Internal, "error querying data from db: %v", err)
-		}
-	} else if len(nonCachedIds) > 0 {
-		// 1 or more cache misses require querying db
-		nonCachedPspsArr, err := s.queryDbForPokemonStockPairs(ctx, nonCachedIds)
-		if err != nil {
-			return nil, status.Errorf(codes.Internal, "error querying data from db: %v", err)
-		}
-
-		nonCachedPspsMap := map[string]*common_pb.PokemonStockPair{}
-		for _, nonCachedPsp := range nonCachedPspsArr {
-			nonCachedPspsMap[fmt.Sprint(nonCachedPsp.Id)] = nonCachedPsp
-		}
-
-		for _, id := range pspIds {
-			val, ok := cachedPspsMap[id]
-			if ok {
-				psps = append(psps, val)
-			} else {
-				psps = append(psps, nonCachedPspsMap[id])
-			}
-		}
-	} else {
-		// 0 cache misses
-		for _, id := range cachedIds {
-			psps = append(psps, cachedPspsMap[id])
-		}
+	psps, err := s.queryPokemonStockPairs(ctx, pspIds)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "error querying PSPs: %v", err)
 	}
 
 	err = s.enrichWithStockPrices(ctx, psps)
