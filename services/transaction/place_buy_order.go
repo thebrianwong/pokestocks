fc21814bc9b5752d36da08d3abd6dc7c26be76b7
Brian Wong <brianw7218@gmail.com>
2024-09-25 11:35:00 -0700

    Start adding buy order rpc

---
+++ services/transaction/place_buy_order.go
@@ -0,0 +1,40 @@
+package transaction
+
+import (
+	"context"
+	"fmt"
+	transaction_pb "pokestocks/proto/transaction"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) PlaceBuyOrder(ctx context.Context, in *transaction_pb.PlaceBuyOrderRequest) (*transaction_pb.PlaceBuyOrderResponse, error) {
+	cm := s.ClientManager
+
+	stockQuantity := float64(in.Quantity)
+
+	psps, err := cm.QueryPokemonStockPairs(ctx, []string{fmt.Sprint(in.PspId)})
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "error querying PSPs: %v", err)
+	}
+
+	err = cm.EnrichWithStockPrices(ctx, psps)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "error querying Alpaca for price data: %v", err)
+	}
+
+	psp := psps[0]
+	stockPrice := *psp.Stock.Price
+
+	totalPrice := stockPrice * stockQuantity
+
+	cash, err := cm.QueryPortfolioCash(ctx, in.PortfolioId)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "error querying portfolio cash: %v", err)
+	}
+
+	hasSufficientCash := cash >= totalPrice
+
+	return &transaction_pb.PlaceBuyOrderResponse{Message: fmt.Sprint(hasSufficientCash)}, nil
+}
