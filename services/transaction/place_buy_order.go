1b4c5a919f9cb6d9c0204ab7653a231541fd3002
Brian Wong <brianw7218@gmail.com>
2024-09-25 12:27:06 -0700

    Add error handling for quantity

--- services/transaction/place_buy_order.go
+++ services/transaction/place_buy_order.go
@@ -25,6 +25,10 @@ func (s *Server) PlaceBuyOrder(ctx context.Context, in *transaction_pb.PlaceBuyO
 	pspId := []string{fmt.Sprint(in.PspId)}
 	quantity := float64(in.Quantity)
 
+	if quantity < 1 {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid quantity argument")
+	}
+
 	psps, err := cm.QueryPokemonStockPairs(ctx, pspId)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "error querying PSPs: %v", err)
